ffcfed0997ec14269a9eb79827a7b9e8c3ae297e
Victor Jean <victorjean@Mini-de-Victor.lan>
2019-09-28 09:25:10 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <script
+            src="https://code.jquery.com/jquery-3.1.1.min.js"
+            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+            crossorigin="anonymous"></script>
+        <script language="javascript">
+			$(document).ready(function()
+			{
+
+			});
+		</script>
+        <style>
+            /* .selectbutton							{background-image:url(img/transpBlack.png);} */
+            body {background-color: turquoise;}
+            .btnbutton								{background-image:url(img/whiteborder.png);cursor:pointer;}
+            .btnbutton:hover						{background-image:url(img/orangeborder.png);cursor:pointer;}
+            .btnbutton:active						{background-image:url(img/greenborder.png);cursor:pointer;}
+            input[type=button]						{color:white;border:3px;padding:5 10;margin:3px;cursor:pointer;}
+            input[type=button]:hover				{color:white;border:3px;padding:5 10;margin:3px;cursor:pointer;}
+            input[type=button]:active				{color:white;border:3px;padding:5 10;margin:3px;cursor:pointer;}
+            input[type=submit]						{color:white;border:3px;padding:5 10;margin:3px;cursor:pointer;}
+            input[type=submit]:hover				{color:white;border:3px;padding:5 10;margin:3px;cursor:pointer;}
+            input[type=submit]:active				{color:white;border:3px;padding:5 10;margin:3px;cursor:pointer;}
+            button									{color:white;border:3px;padding:5 10;margin:3px;cursor:pointer;}
+            button:hover							{color:white;border:3px;padding:5 10;margin:3px;cursor:pointer;}
+            button:active							{color:white;border:3px;padding:5 10;margin:3px;cursor:pointer;}
+        </style>
+    </head>
+    <body>
+        <div class="btnbutton" style="display:inline-block;"><input type="button" value="Cancelar Cadastro" style="background:#ffc0cb;"></div>
+        <div class="btnbutton" style="display:inline-block;"><input type="button" value="Continuar Cadastro" style="background:#b22222;"></div>
+    </body>
+</html>
